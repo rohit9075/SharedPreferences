a56c345f426b762f60a4a7948a989a2414d0e6cf
rohit9075 <rohit9075@gmail.com>
2018-02-16 17:45:39 +0530

    Initial commit

---
+++ app/src/main/java/rohit/com/sharedpreferences/HomeActivity.java
@@ -0,0 +1,13 @@
+package rohit.com.sharedpreferences;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_home);
+    }
+}
